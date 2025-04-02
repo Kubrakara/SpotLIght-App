0129b3b1899bc956a6cec8390f6feff981c75bfa
Kubrakara <kubra26kara@gmail.com>
2025-04-02 16:32:57 +0300

    Add Loader and Story components; implement user authentication and feed posts retrieval

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,78 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import { styles } from "../../styles/auth.styles";
+import {
+  FlatList,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { styles } from "../../styles/feed.styles";
 import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants/theme";
+import { STORIES } from "@/constants/mock-data";
+import Story from "@/components/Story";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import Post from "@/components/Post";
+import { Loader } from "@/components/Loader";
 
 export default function Index() {
   const { signOut } = useAuth();
+  const posts = useQuery(api.posts.getFeedPosts);
 
-  const handleSignOut = async () => {
-    try {
-      await signOut();
-      console.log("Sign out successful!");
-    } catch (error) {
-      console.error("Sign out failed:", error);
-    }
-  };
+  if (posts === undefined) return <Loader />;
+
+  if (posts.length === 0) return <NoPostsFound />;
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={handleSignOut}>
-        <Text style={{ color: "white" }}>Sign Out</Text>
-      </TouchableOpacity>
+      <View style={styles.header}>
+        {/* HEADER */}
+        <Text style={styles.headerTitle}>Spotlight</Text>
+        <TouchableOpacity onPress={() => signOut()}>
+          <Ionicons
+            name="log-out-outline"
+            size={24}
+            color={COLORS.white}
+          ></Ionicons>
+        </TouchableOpacity>
+      </View>
+ 
+
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => <Post post={item} />}
+        keyExtractor={(item) => item._id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 60 }}
+        ListHeaderComponent={<StoriesSection />}
+      />
     </View>
   );
 }
+const StoriesSection = () => {
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={true}
+      style={styles.storiesContainer}
+    >
+      {STORIES.map((story) => (
+        <Story key={story.id} story={story}></Story>
+      ))}
+    </ScrollView>
+  );
+};
+
+const NoPostsFound = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: COLORS.background,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Text style={{ fontSize: 20, color: COLORS.primary }}>No posts yet</Text>
+  </View>
+);
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,27 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import InitialLayout from "@/components/InitialLayout";
 import ClerckAndConvexProviders from "@/providers/ClerckAndConvexProviders";
+import { SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
   return (
     <ClerckAndConvexProviders>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+        <SafeAreaView
+          style={{ flex: 1, backgroundColor: "#000" }}
+          onLayout={onLayoutRootView}
+        >
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
